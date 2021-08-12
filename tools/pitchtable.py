428bc98819f42a0620d32b33ccb15ca82f6f8bd3
Damian Yerrick <git@pineight.com>
2021-08-12 13:56:21 -0400

    correct for concert pitch
    tbsp pointed out that the table produces A=441.5 Hz,
    as opposed to concert pitch which is A=440 Hz

--- tools/pitchtable.py
+++ tools/pitchtable.py
@@ -2,7 +2,7 @@
 
 print('section "pitch_table",ROM0,align[1]')
 print("pitch_table::")
-period = 131072.0 / 65.625
+period = 131072.0 / 65.406
 for i in range(64):
     print(" dw %d" % round(2048 - period))
     period = period / 1.059463094
